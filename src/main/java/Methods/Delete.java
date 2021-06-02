33ab2ea51ee916d2bb4d7faa71136b953475f6fc
divya kuthadi <divya.k@gmail.com>
2021-06-02 01:04:32 -0400

    Assessment Project1

---
+++ src/main/java/Methods/Delete.java
@@ -0,0 +1,23 @@
+package Methods;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Delete {
+	@Test
+	public void DeletingResponse() {
+		RestAssured.baseURI="http://dummy.restapiexample.com";
+		RequestSpecification httprequest =RestAssured.given();
+		Response response = httprequest.request(Method.DELETE, "/api/v1/delete/2");
+		String REsponseJson=response.body().asString();
+		System.out.println("the fetched Response is: " +REsponseJson);
+		
+	//when i try to run the script in postman, it showing the error "{"message":"Error Occured! Page Not found, contact rstapi2example@gmail.com"}".	
+
+	
+	}
+}
---
+++ src/main/java/Methods/GetMethod.java
@@ -0,0 +1,43 @@
+package Methods;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetMethod {
+	@Test
+	public void GettingResponse() {
+		RestAssured.baseURI="http:http://dummy.restapiexample.com";
+		RequestSpecification httprequest =RestAssured.given();
+		Response response = httprequest.request(Method.GET, "/api/v1/employees");
+		String REsponseJson=response.body().asString();
+		System.out.println("the fetched Response is: " +REsponseJson);
+		
+		int statuscode=response.statusCode();
+		System.out.println("the status code is: " +statuscode);
+		Assert.assertEquals("200 ok", statuscode);
+
+	
+	}
+	
+	//fetching one record
+	@Test
+	public void GettingOneRecordResponse() {
+		RestAssured.baseURI="http:http://dummy.restapiexample.com";
+		RequestSpecification httprequest =RestAssured.given();
+		Response response = httprequest.request(Method.GET, "/api/v1/employees/6");
+		String REsponseJson=response.body().asString();
+		System.out.println("the fetched Response is: " +REsponseJson);
+		
+		int statuscode=response.statusCode();
+		System.out.println("the status code is: " +statuscode);
+		Assert.assertEquals("404 page not found", statuscode);
+
+	
+	}
+	
+}
